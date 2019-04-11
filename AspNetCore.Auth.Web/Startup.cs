6fc9c53757407fd5b9146d08bb12afa3e4f4946a
= <=>
2019-04-11 22:02:32 +0200

    Creating a User Service

--- AspNetCore.Auth.Web/Startup.cs
+++ AspNetCore.Auth.Web/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AspNetCore.Auth.Web.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +27,9 @@ namespace AspNetCore.Auth.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            var users = new Dictionary<string, string> { { "Chris", "password" } };
+            services.AddSingleton<IUserService>(new DummyUserService(users));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
